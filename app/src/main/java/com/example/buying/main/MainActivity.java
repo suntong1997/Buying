82ddec9eb2f307a70dd2973be9718d3ebb4ba3a8
suntong <189964214@qq.com>
2019-11-10 11:16:45 +0800

    完成了HomeFragment标题布局和样式并优化了表格样式布局

--- app/src/main/java/com/example/buying/main/MainActivity.java
+++ app/src/main/java/com/example/buying/main/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.buying.main;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
 import com.example.buying.R;
+import com.example.buying.login.fragment.LoginFragment;
+import com.example.buying.main.fragment.HomeFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +15,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*关闭标题栏*/
+        ActionBar actionBar=getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+        getSupportFragmentManager().beginTransaction().add(R.id.main_fragment, HomeFragment.newInstance()).addToBackStack(null).commit();
     }
 }
